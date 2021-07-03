0eeca02d63b07dae5a4bfa2ebb8bd949f7af49e2
Felix Maldonado <felixjmt@gmail.com>
2021-07-03 23:25:26 +0000

    Personal portfolio updated

--- portfolio/src/main/webapp/script2.js
+++
@@ -1,9 +0,0 @@
-/* Javascript function that displays the current date and time in page */
-async function displayTime(){
-
-    const responseFromServer = await fetch('/date');
-    const textFromResponse = await responseFromServer.text();
-
-    const dateContainer = document.getElementById('date-container');
-    dateContainer.innerText = textFromResponse;
-}
